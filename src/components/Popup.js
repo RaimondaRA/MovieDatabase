dcaeedf57ca218538b7fd9135ffc215deac580af
RaimondaRA <raimonda.alisauskaite@gmail.com>
2021-05-15 00:14:41 +0300

    Pakoreguota, veikia, be pop-up lango

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,10 +5,18 @@ function Popup({selected, closePopup}) {
         <section className="popup">
             <div className="content">
                 <h2>{selected.Title}<span>({selected.Year})</span></h2>
+                <p className="genre">{selected.Genre}</p>
                 <p className="rating">Rating: {selected.imdbRating}</p>
                 <div className="plot">
-                    <img src={selected.Poster}/>
-                    <p>{selected.Plot}</p>
+                     <img src={selected.Poster}/>
+                    <p>{selected.Plot}<span>({selected.Runtime})</span></p>
+                </div>
+                <div>
+                    
+                    <p>Director: {selected.Director}</p>
+                    <p className="actors">Actors: {selected.Actors}</p>
+                    <p>Language: {selected.Language}</p>
+                    <p>Country: {selected.Country}</p>    
                 </div>
                 <button className="close" onClick={closePopup}>Close</button>
             </div>
